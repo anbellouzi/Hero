9e8a7fa9f762df41c011f0d0d933faf1d839369f
Anas <com.anas.idms.appleid.prd.001784-08-5d674918-865b-4553-9bf0-c2326b981e34@AB-Macbook.local>
2019-09-23 15:30:06 -0700

    Moved all code into 1 file in hero.py

--- hero.py
+++ hero.py
@@ -1,4 +1,27 @@
-import ability
+import random
+class Ability:
+    # Required properties are defined inside the __init__ constructor method
+    def __init__(self, name, attack_strength):
+        self.name = name
+        self.attack_strength = attack_strength
+
+    def attack(self):
+        attack = random.randint(0, self.attack_strength)
+        return attack
+
+
+import random
+class Armor:
+    # Required properties are defined inside the __init__ constructor method
+    def __init__(self, name, max_block):
+        self.name = name
+        self.max_block = max_block
+
+
+    def block(self):
+        block = random.randint(0, self.max_block)
+        return block
+
 
 # Superheros.py
 class Hero:
@@ -6,36 +29,47 @@ class Hero:
     def __init__(self, name, starting_health=100):
         self.name = name
         self.current_health = starting_health
+        self.abilities = list()
+        self.armors = list()
+        self.starting_health = starting_health
 
-    abilities = []
+
 
     def add_ability(self, ability):
         self.abilities.append(ability)
-        pass
+
+    def add_armor(self, armor):
+        self.armors.append(armor)
 
 
-    def attack():
-        pass
+    def attack(self):
+        '''Calculate the total damage from all ability attacks.
+          return: total:Int
+        '''
+        # TODO: This method should run Ability.attack() on every ability
+        # in self.abilities and returns the total as an integer.
+        damage = 0
+        for ability in self.abilities:
+            damage += ability.attack()
 
-    def defend(incoming_damage):
-        pass
+        return damage
 
-    def take_damage(damage):
-        pass
+    def defand(self, damage_amt):
+        '''Runs `block` method on each armor. Returns sum of all blocks'''
+        # TODO: This method should run the block method on each armor in self.armors
+        sum = 0
 
-    def is_alive():
-        pass
-
-    def fight(opponent):
-        pass
-
-
+        if armors:
+            for armor in armors:
+                sum += armor.block()
 
 
 if __name__ == "__main__":
     # If you run this file from the terminal
-    # this block is executed.
-    ability = ability.Ability("Great Debugging", 50)
+    # this block of code is executed.
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
     hero = Hero("Grace Hopper", 200)
     hero.add_ability(ability)
-    print(hero.abilities)
+    hero.add_ability(another_ability)
+    print(hero.attack())
