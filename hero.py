9655a40437abd95f88c598de7df6523f9a445638
Anas <com.anas.idms.appleid.prd.001784-08-5d674918-865b-4553-9bf0-c2326b981e34@AB-Macbook.local>
2019-09-23 20:27:25 -0700

    added figth() and files for OOP section

--- hero.py
+++ hero.py
@@ -26,7 +26,7 @@ class Armor:
 # Superheros.py
 class Hero:
     # Required properties are defined inside the __init__ constructor method
-    def __init__(self, name, starting_health):
+    def __init__(self, name, starting_health=100):
         self.name = name
         self.starting_health = starting_health
         self.current_health = self.starting_health
@@ -41,12 +41,23 @@ class Hero:
     def get_current_health(self):
         return self.current_health
 
+    # getter for armors
     def get_armors(self):
         return self.armors
 
+    # getter for abilities
+    def get_abilities(self):
+        return self.abilities
+
+    # getter for name
+    def get_name(self):
+        return self.name
+
+    # add ability
     def add_ability(self, ability):
         self.abilities.append(ability)
 
+    # add armors
     def add_armor(self, armor):
         self.armors.append(armor)
 
@@ -58,8 +69,13 @@ class Hero:
         # TODO: This method should run Ability.attack() on every ability
         # in self.abilities and returns the total as an integer.
         total_damage = 0
-        for ability in self.abilities:
-            total_damage += ability.attack()
+        abilities = self.get_abilities()
+        if abilities:
+            for ability in self.abilities:
+                total_damage += ability.attack()
+
+        else:
+            print(self.get_name()+" has no abilities!")
 
         return total_damage
 
@@ -72,9 +88,7 @@ class Hero:
             for armor in armors:
                 total_armors += armor.block()
 
-
         return damage_amt - total_armors
-
 
     def take_damage(self, damage):
         '''Updates self.current_health to reflect the damage minus the defense.
@@ -85,19 +99,51 @@ class Hero:
         self.set_current_health(new_health)
 
     def is_alive(self):
-      '''Return True or False depending on whether the hero is alive or not.
-      '''
-      # TODO: Check whether the hero is alive and return true or false
-      pass
+        '''Return True or False depending on whether the hero is alive or not.
+        '''
+        # TODO: Check whether the hero is alive and return true or false
+        if self.get_current_health() <= 0:
+          return False
+        else:
+          return True
+
+
+    def fight(self, opponent):
+        ''' Current Hero will take turns fighting the opponent hero passed in.
+        '''
+        # TODO: Fight each hero until a victor emerges.
+        # Print the victor's name to the screen.
+        while True:
+            if self.get_abilities() and opponent.get_abilities():
+                if self.is_alive():
+                    opponent.take_damage(self.attack())
+                else:
+                    print(opponent.get_name()+" Won!")
+                    return False
+
+                if opponent.is_alive():
+                    self.take_damage(opponent.attack())
+                else:
+                    print(self.get_name()+" Won!")
+                    return False
+            else:
+                print("Draw")
+                return False
+
 
 
 
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block of code is executed.
-    hero = Hero("Grace Hopper", 200)
-    print("Hero current health before: "+str(hero.current_health))
-    shield = Armor("Shield", 50)
-    hero.add_armor(shield)
-    hero.take_damage(50)
-    print("Hero current health after: "+str(hero.current_health))
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
---
+++ my-dog.py
@@ -0,0 +1,10 @@
+import dog
+
+my_dog1 = dog.Dog("Rex", "SuperDog")
+my_dog2 = dog.Dog("Annie", "LazyDog")
+my_dog3 = dog.Dog("Castor", "FunDog")
+
+
+my_dog1.bark()
+my_dog2.sit()
+my_dog3.roll()
